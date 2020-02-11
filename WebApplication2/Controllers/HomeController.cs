1c6a2586bd71d460493887098f039181ce6182ea
jblackwood12 <justinedwardblackwood@gmail.com>
2020-02-10 20:49:24 -0500

    Implement AngularJS version.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View(new ScheduleCustomerModel());
+		}
+	}
+}
---
+++ WebApplication2/Models/ScheduledCustomerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+	public class ScheduledCustomerModel
+	{
+		public int AppointmentID { get; set; }
+		public string Name { get; set; }
+		public string PhoneNumber { get; set; }
+		public string BarberNameRequested { get; set; }
+		public string CurrentBarberAssigned { get; set; }
+		public decimal WaitTime { get; set; }
+	}
+}
